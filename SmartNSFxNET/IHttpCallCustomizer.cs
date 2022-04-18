6899a12f650b7e6f5ab81c2e3e366610fb50941a
Christian Güdemann <christian.guedemann@webgate.biz>
2022-04-18 13:44:45 +0200

    Updated Project Dependencies

--- SmartNSFxNET/IHttpCallCustomizer.cs
+++
@@ -1,9 +0,0 @@
-using System.Net.Http;
-namespace OpenNTF.SmartNSFxNET
-{
-    public interface IHttpCallCustomizer
-    {
-        void OnClientCreate(HttpClient client);
-        void BeforeHttpCall(HttpRequestMessage requestMessage);
-    }
-}
